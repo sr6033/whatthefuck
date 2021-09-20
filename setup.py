16dd3a9a3a6656874c72e9c7f8f3ef91420cf866
Shubham <shubham.rath@go-mmt.com>
2021-09-20 22:16:04 +0530

    Add files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'whatthefuck',
+    packages = ['whatthefuck'],
+    version = 'v1.0',  # Ideally should be same as your github release tag varsion
+    description = 'Frustration made me develop this script.',
+    author = 'Shubham Rath',
+    author_email = 'shubhamrath.iiitb@gmail.com',
+    url = 'https://github.com/sr6033/whatthefuck',
+    download_url = 'https://github.com/sr6033/whatthefuck/archive/refs/tags/v1.0.tar.gz',
+    keywords = ['v1.0'],
+    classifiers = [],
+)
