3c471c0634ec6219d5b97615c5cd7d99cc3f87cf
Shubham <shubham.rath@go-mmt.com>
2021-09-20 23:10:08 +0530

    Add file changes

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'whatthefuck',
-    packages = ['whatthefuck'],
-    version = 'v1.0',  # Ideally should be same as your github release tag varsion
-    description = 'Frustration made me develop this script.',
-    author = 'Shubham Rath',
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="whatthefuck",                     # This is the name of the package
+    version="v1.1",                        
+    author = 'Shubham Rath',                     # Full name of the author
     author_email = 'shubhamrath.iiitb@gmail.com',
-    url = 'https://github.com/sr6033/whatthefuck',
-    download_url = 'https://github.com/sr6033/whatthefuck/archive/refs/tags/v1.0.tar.gz',
-    keywords = ['v1.0'],
-    classifiers = [],
+    description = 'Frustration made me develop this script.',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.5',                # Minimum version requirement of the package
+    py_modules=["whatthefuck"],             # Name of the python package
+    package_dir={'':'whatthefuck/src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
 )
